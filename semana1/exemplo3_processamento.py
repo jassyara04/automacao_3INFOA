ca3c3872f927ea9e8c22c5a64d2782afe2552aad
Jassyára de Cássia <jassyaracassiapereiradacosta@gmail.com>
2024-02-19 10:13:53 -0300

    Aula da semana 1

---
+++ semana1/exemplo3_processamento.py
@@ -0,0 +1,13 @@
+#entrada
+n1 = input("Digite o número 1: ")
+n2 = input("Digite o número 2: ")
+
+#processamento
+soma = int(n1) + int(n2)
+
+#saída
+print("A soma é de ", n1, " + ", n2, " = ", soma)
+
+#int("10") -> converte o texto "10" para inteiro 10
+#float("1.5") -> converte o texto "1.5" para real 1.5
+#str(10) -> converte o inteiro 10 para texto "10"
